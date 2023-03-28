ceed94c3efc7caac94fa0e4124033f1743dc89b2
skdding3 <skdding3@gmail.com>
2023-03-28 18:06:40 +0900

    jest test code, script 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+export type Props = {
+  name: string;
+};
+
+function Home({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>클라이밍 하고싶다 !!!</Text>
+      <Button title="상세" onPress={() => navigation.navigate("Details")} />
     </View>
+  );
+}
+
+function Detail({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>클라이밍!!!!!!🧗🧗🧗🧗</Text>
+      <Button title="뒤로가기" onPress={() => navigation.navigate("Main")} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={Home}
+          options={{ title: "메인" }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Detail}
+          options={{ title: "클친자" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
